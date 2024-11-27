26b44e5c32fc1bdf0040cf0c9679cbc44c3c4573
Sanesu <118468259+SanesuOff@users.noreply.github.com>
2024-11-27 10:54:35 +0100

    main

--- data/script.js
+++ data/script.js
@@ -1,5 +1,3 @@
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
 function onButton() { 
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", "on", true);
@@ -11,45 +9,3 @@ function offButton() {
     xhttp.open("GET", "off", true);
     xhttp.send();
 }
-
-setInterval(function getData()
-{
-    var xhttp = new XMLHttpRequest();
-
-    xhttp.onreadystatechange = function()
-    {
-        if(this. readyState == 4 && this.status == 200)
-        {
-            document.getElementById("analogv").innerHTML = this.responseText;
-        }
-    };
-
-    xhttp.open("GET", "read-analogv", true);
-    xhttp.send();
-}, 20);
-
-
-var ctx = document.getElementById("hchart").getContext('2d')
-
-const labels = ['1', '2', '3', '4'];
-
-var data = {
-    labels: labels,
-    datasets: [
-        {
-            label: "ElectroCardiogramme",
-            data: []
-        }
-    ]
-}
-
-var options = {
-    resonsive: false
-}
-
-var config = {
-    type: 'line',
-    data: data,
-    options: options
-}
-var hchart = new Chart(ctx, config)
--- src/main.cpp
+++ src/main.cpp
@@ -6,25 +6,30 @@
 #include <Adafruit_ADS1X15.h>
 #include <Adafruit_I2CDevice.h>
 #include <SPI.h>
+#include <WiFiClientSecure.h>
+#include <WebSocketsServer.h>
 
 Adafruit_ADS1115 ads;
 
-const char* wifi_network_ssid     = "Wifi_SSID";
+// * Wifi to connect
+const char* wifi_network_ssid     = "Sanesu's Box";
 const char* wifi_network_password =  "";
 
+// * Wifi to create
 const char* soft_ap_ssid          = "ESP_Wifi";
 const char* soft_ap_password      = "9839*-56m";
 
 const int led = 2;
-const int analogv = 34;
+const int analogv = 39;
 
 AsyncWebServer server(80);
+WebSocketsServer webSocket = WebSocketsServer(81);  // WebSocket server on port 81
 
 void setup() 
 {
     //------------------------------------Serial
-    Serial.begin(9600);
-    //while(!Serial){}  //A retirer a la fin du dev
+    Serial.begin(115200);
+    while(!Serial){}  //A retirer a la fin du dev
     Serial.println("\n");
 
     //------------------------------------ADS
@@ -63,22 +68,24 @@ void setup()
     
     WiFi.mode(WIFI_AP_STA);
 
-    Serial.println("\n[*] Creation du Point d'accès ESP32");
-    WiFi.softAP(soft_ap_ssid, soft_ap_password);
-    Serial.print("[+] Point d'accès crée ayant pour passerelle ");
-    Serial.println(WiFi.softAPIP());
-
     WiFi.begin(wifi_network_ssid, wifi_network_password);
-    Serial.println("\n[*] Connexion au réseau WiFi");
+    Serial.println("\n[!] Connexion au réseau WiFi");
 
     while(WiFi.status() != WL_CONNECTED)
     {
-        Serial.print(".");
-        delay(100);
+
+        String ssid = String(wifi_network_ssid);
+        Serial.println(""); Serial.print("[!]Recherche de '"); Serial.print(ssid); Serial.print("' en cours ...");
+        delay(10000);
     }
 
-    Serial.print("\n[+] Connecté au réseau WiFi avec l'IP locale : ");
+    Serial.println(""); Serial.print("\n[+] Connecté au réseau WiFi avec l'IP locale : ");
     Serial.println(WiFi.localIP());
+
+    Serial.println("\n[!] Creation du Point d'accès ESP32");
+    WiFi.softAP(soft_ap_ssid, soft_ap_password);
+    Serial.print("[+] Point d'accès crée ayant pour passerelle :");
+    Serial.println(WiFi.softAPIP());
     
     //------------------------------------SERVER
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
@@ -86,31 +93,51 @@ void setup()
         request->send(SPIFFS, "/index.html", "text/html");
     });
 
-    server.on("/w3.css", HTTP_GET, [](AsyncWebServerRequest *request)
+    server.on("/base.css", HTTP_GET, [](AsyncWebServerRequest *request)
     {
-        request->send(SPIFFS, "/w3.css", "text/css");
+        request->send(SPIFFS, "/base.css", "text/css");
     });
 
     server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest *request)
     {
         request->send(SPIFFS, "/script.js", "text/javascript");
     });
-
-    server.on("/read-analogv", HTTP_GET, [](AsyncWebServerRequest *request)
+    server.on("/chart.js", HTTP_GET, [](AsyncWebServerRequest *request)
     {
+        request->send(SPIFFS, "/chart.js", "text/javascript");
+    });
 
+    /*server.on("/read-analogv", HTTP_GET, [](AsyncWebServerRequest *request)
+    {
+        //Serial.print("AIN0-cor: "); Serial.print(value); Serial.print("  "); Serial.print(volts); Serial.println("V");
         int16_t adc;
         float volts;
         int value;
         adc = ads.readADC_SingleEnded(0);
         volts = ads.computeVolts(adc);
-        value = map(adc, 0, 26920, 0, 32765);
-        
-        Serial.print("AIN0-cor: "); Serial.print(value); Serial.print("  "); Serial.print(volts); Serial.println("V");
-    
+        value = map(adc, 0, 3.3, 0, 16382);
+
+        if (value < 0) {
+            value = 0;
+        }else if (value > 32765) {
+            value = 32765;
+        }
+
         int val = value;
         String analogv = String(val);
         request->send(200, "text/plain", analogv);
+    });*/
+
+    server.on("/ecg", HTTP_GET, [](AsyncWebServerRequest *request)
+    {
+        int16_t adc;
+        int value;
+        adc = ads.readADC_SingleEnded(0);
+        value = adc;
+    
+        String analval = String(value);
+        
+        request->send(200, "text/plain", analval);
     });
 
     server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request)
@@ -123,20 +150,20 @@ void setup()
     {
         digitalWrite(led, LOW);
         request->send(200);
-    });
-
+    }); 
+    
     server.begin();
     Serial.println("Serveur actif!");
 }
 
-void loop() 
-{
-    int16_t adc;
-    float volts;
+void loop() {
+    /*int16_t adc;
     int value;
     adc = ads.readADC_SingleEnded(0);
-    volts = ads.computeVolts(adc);
-    value = map(adc, 0, 26920, 0, 32765);
-        
-    Serial.print("AIN0-cor: "); Serial.print(value); Serial.print("  "); Serial.print(volts); Serial.println("V");
+    value = adc;
+    
+    Serial.print(">ECG: ");
+    Serial.println(value);*/
+    
+    //Compter le temps entre deux valeur(adc) en dessous de 2000 afins de déterminer le nombre de ms ou s pour 1 battemenr pour ensuite répéter cette action 6 fois et pouvoir faire *10 pour avoir le nombre de battement par minute
 }
