9060d6abf01e6e101900aa47ae0c946d9a253563
Sanesu <118468259+SanesuOff@users.noreply.github.com>
2025-02-05 09:07:58 +0100

    main

--- src/main.cpp
+++ src/main.cpp
@@ -7,163 +7,101 @@
 #include <Adafruit_I2CDevice.h>
 #include <SPI.h>
 #include <WiFiClientSecure.h>
-#include <WebSocketsServer.h>
 
 Adafruit_ADS1115 ads;
 
-// * Wifi to connect
-const char* wifi_network_ssid     = "Sanesu's Box";
-const char* wifi_network_password =  "";
-
-// * Wifi to create
-const char* soft_ap_ssid          = "ESP_Wifi";
-const char* soft_ap_password      = "9839*-56m";
-
 const int led = 2;
-const int analogv = 39;
 
+// Décommenter et utiliser le serveur asynchrone
 AsyncWebServer server(80);
-WebSocketsServer webSocket = WebSocketsServer(81);  // WebSocket server on port 81
 
-void setup() 
+// Utiliser uniquement le mode Station (pas AP)
+const char *wifi_network_ssid = "Wifi SIN";
+const char *wifi_network_password = "";
+
+const int hallSensorPin = 27;
+volatile unsigned long lastPulseTime = 0;
+volatile unsigned long pulseInterval = 0;
+volatile bool newMeasurement = false;
+
+// Constantes pour les calculs
+const int MAGNETS_PER_REVOLUTION = 9; // Nombre d'aimants positifs (18 total / 2)
+const float RADIUS_METERS = 0.1; // Rayon en mètres (à ajuster selon votre système)
+
+void IRAM_ATTR handleHallSensor() {
+    unsigned long currentTime = micros();
+    pulseInterval = currentTime - lastPulseTime;
+    lastPulseTime = currentTime;
+    newMeasurement = true;
+}
+
+void setup()
 {
-    //------------------------------------Serial
-    Serial.begin(115200);
-    while(!Serial){}  //A retirer a la fin du dev
-    Serial.println("\n");
-
-    //------------------------------------ADS
-    ads.setGain(GAIN_TWOTHIRDS); 
-
-    if (!ads.begin())
-    {
-        Serial.println("Failed to initialize ADS.");
+    Serial.begin(9600);
+    
+    // Configuration de l'ADS1115
+    ads.setGain(GAIN_TWOTHIRDS);
+    if (!ads.begin()) {
+        Serial.println("Échec de l'initialisation ADS.");
         while (1);
     }
 
-    //------------------------------------GPIO
-    pinMode(led, OUTPUT);
-    digitalWrite(led, LOW);
-    pinMode(analogv, INPUT);
+    // Configuration du capteur a effet hall
+    pinMode(hallSensorPin, INPUT);
+    attachInterrupt(digitalPinToInterrupt(hallSensorPin), handleHallSensor, RISING);
 
-    //------------------------------------SPIFFS
-    if(!SPIFFS.begin())
-    {
+    // Initialisation du SPIFFS
+    if (!SPIFFS.begin()) {
         Serial.println("Erreur SPIFFS...");
         return;
     }
 
-    File root = SPIFFS.open("/");
-    File file = root.openNextFile();
-
-    while(file)
-    {
-        Serial.print("File: ");
-        Serial.println(file.name());
-        file.close();
-        file = root.openNextFile();
-    }
-
-    //------------------------------------WIFI
-    
-    WiFi.mode(WIFI_AP_STA);
-
+    // Configuration du WiFi en mode Station
+    WiFi.mode(WIFI_STA);
     WiFi.begin(wifi_network_ssid, wifi_network_password);
-    Serial.println("\n[!] Connexion au réseau WiFi");
-
-    while(WiFi.status() != WL_CONNECTED)
-    {
-
-        String ssid = String(wifi_network_ssid);
-        Serial.println(""); Serial.print("[!]Recherche de '"); Serial.print(ssid); Serial.print("' en cours ...");
-        delay(10000);
+    Serial.println("\nConnexion au réseau WiFi");
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
     }
-
-    Serial.println(""); Serial.print("\n[+] Connecté au réseau WiFi avec l'IP locale : ");
+    Serial.print("\nConnecté à l'IP: ");
     Serial.println(WiFi.localIP());
 
-    Serial.println("\n[!] Creation du Point d'accès ESP32");
-    WiFi.softAP(soft_ap_ssid, soft_ap_password);
-    Serial.print("[+] Point d'accès crée ayant pour passerelle :");
-    Serial.println(WiFi.softAPIP());
-    
-    //------------------------------------SERVER
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-    {
+    // Configuration des routes du serveur
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
         request->send(SPIFFS, "/index.html", "text/html");
     });
-
-    server.on("/base.css", HTTP_GET, [](AsyncWebServerRequest *request)
-    {
-        request->send(SPIFFS, "/base.css", "text/css");
-    });
-
-    server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest *request)
-    {
-        request->send(SPIFFS, "/script.js", "text/javascript");
-    });
-    server.on("/chart.js", HTTP_GET, [](AsyncWebServerRequest *request)
-    {
-        request->send(SPIFFS, "/chart.js", "text/javascript");
-    });
-
-    /*server.on("/read-analogv", HTTP_GET, [](AsyncWebServerRequest *request)
-    {
-        //Serial.print("AIN0-cor: "); Serial.print(value); Serial.print("  "); Serial.print(volts); Serial.println("V");
-        int16_t adc;
-        float volts;
-        int value;
-        adc = ads.readADC_SingleEnded(0);
-        volts = ads.computeVolts(adc);
-        value = map(adc, 0, 3.3, 0, 16382);
-
-        if (value < 0) {
-            value = 0;
-        }else if (value > 32765) {
-            value = 32765;
-        }
-
-        int val = value;
-        String analogv = String(val);
-        request->send(200, "text/plain", analogv);
-    });*/
-
-    server.on("/ecg", HTTP_GET, [](AsyncWebServerRequest *request)
-    {
-        int16_t adc;
-        int value;
-        adc = ads.readADC_SingleEnded(0);
-        value = adc;
     
-        String analval = String(value);
-        
-        request->send(200, "text/plain", analval);
+    server.on("/data", HTTP_GET, [](AsyncWebServerRequest *request) {
+        int16_t adc = ads.readADC_SingleEnded(0);
+        String valeur = String(adc);
+        request->send(200, "text/plain", valeur);
     });
-
-    server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request)
-    {
-        digitalWrite(led, HIGH);
-        request->send(200);
-    });
-
-    server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request)
-    {
-        digitalWrite(led, LOW);
-        request->send(200);
-    }); 
     
     server.begin();
-    Serial.println("Serveur actif!");
 }
 
-void loop() {
-    /*int16_t adc;
-    int value;
-    adc = ads.readADC_SingleEnded(0);
-    value = adc;
-    
-    Serial.print(">ECG: ");
-    Serial.println(value);*/
-    
-    //Compter le temps entre deux valeur(adc) en dessous de 2000 afins de déterminer le nombre de ms ou s pour 1 battemenr pour ensuite répéter cette action 6 fois et pouvoir faire *10 pour avoir le nombre de battement par minute
+void loop()
+{
+    if (newMeasurement && pulseInterval > 0) {
+        // Calcul de la vitesse
+        float rps = 1000000.0 / (pulseInterval * MAGNETS_PER_REVOLUTION); // Tours par seconde
+        float rpm = rps * 60.0; // Tours par minute
+        float mps = rps * (2 * PI * RADIUS_METERS); // Mètres par seconde
+
+        Serial.print("Vitesse: ");
+        Serial.print(rpm, 2); // 2 décimales
+        Serial.print(" tpm | ");
+        Serial.print(mps, 2);
+        Serial.println(" m/s");
+
+        newMeasurement = false;
+    }
+
+    /* Système de Plotter via Cursor */
+    // int16_t adc;
+    // int value;
+    // adc = ads.readADC_SingleEnded(0);
+    // value = adc;
+    // Serial.print(">ECG: "); Serial.println(value);
 }
